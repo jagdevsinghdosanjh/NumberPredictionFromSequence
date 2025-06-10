052b1e2c0c109d85a48292cc409360603e895702
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-06-10 13:34:54 +0530

    Ok

--- Ver1.1/app.py
+++ Ver1.1/app.py
@@ -5,45 +5,6 @@ from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 
-# Dummy function for pattern recognition & prediction
-# def predict_sequence(sequence):
-#     sequence = np.array(sequence).reshape(-1, 1)
-#     indices = np.arange(len(sequence)).reshape(-1, 1)
-
-#     model = LinearRegression()
-#     model.fit(indices, sequence)
-
-#     future_indices = np.arange(len(sequence), len(sequence) + 5).reshape(-1, 1)
-#     predictions = model.predict(future_indices)
-    
-
-#     return predictions.flatten().tolist()
-# def predict_sequence(sequence):
-#     sequence = np.array(sequence).reshape(-1, 1)
-#     indices = np.arange(len(sequence)).reshape(-1, 1)
-
-#     model = LinearRegression()
-#     model.fit(indices, sequence)
-
-#     future_indices = np.arange(len(sequence), len(sequence) + 5).reshape(-1, 1)
-#     predictions = model.predict(future_indices)
-
-#     # Convert to integer format by rounding
-#     return [round(num) for num in predictions]
-
-# def predict_sequence(sequence):
-#     sequence = np.array(sequence).reshape(-1, 1)
-#     indices = np.arange(len(sequence)).reshape(-1, 1)
-
-#     model = LinearRegression()
-#     model.fit(indices, sequence)
-
-#     future_indices = np.arange(len(sequence), len(sequence) + 5).reshape(-1, 1)
-#     predictions = model.predict(future_indices)
-
-#     # Convert NumPy array to a list, then round each number
-#     return [round(float(num)) for num in predictions.tolist()]
-
 def predict_sequence(sequence):
     sequence = np.array(sequence).reshape(-1, 1)
     indices = np.arange(len(sequence)).reshape(-1, 1)
@@ -56,18 +17,6 @@ def predict_sequence(sequence):
 
     # Ensure proper list conversion before rounding
     return [round(float(num)) for num in predictions.flatten()]
-
-
-
-# @app.route("/", methods=["GET", "POST"])
-# def index():
-#     if request.method == "POST":
-#         sequence = list(map(float, request.form.get("sequence").split(",")))
-#         predictions = predict_sequence(sequence)
-#         return render_template("index.html", predictions=predictions)
-
-#     #return render_template("index.html")
-#     return render_template("index.html", predictions=[round(num, 2) for num in predictions])
 
 @app.route("/", methods=["GET", "POST"])
 def index():
